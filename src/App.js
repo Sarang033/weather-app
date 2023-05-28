e525e2b1d4edf46449a2df59209e363dc845964f
Sarang Sharma <sarangchamp2--4>
2023-05-28 18:51:43 +0530

    UI

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Search from './components/search/Search';
+import './App.css';
+
+function App() {
+
+  const handleOnSearchChange=(searchData)=>{
+    console.log(searchData);
+  }
+
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,11 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '37280cdba1msh48dbb18301ae63ap15286fjsnc170e2e84cbc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
